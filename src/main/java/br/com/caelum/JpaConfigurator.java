2012a147dec3bb78f59b0c47de9a6b72aef2a95b
Rafael <rafaelg202@gmail.com>
2018-06-24 13:05:36 -0300

    statistics

--- src/main/java/br/com/caelum/JpaConfigurator.java
+++ src/main/java/br/com/caelum/JpaConfigurator.java
@@ -6,6 +6,8 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -43,6 +45,13 @@ public class JpaConfigurator {
 
 	    return dataSource;
 	}
+	
+	@Bean
+	public Statistics statistics(EntityManagerFactory emf) {
+		
+		return emf.unwrap(SessionFactory.class).getStatistics();
+		
+	}
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource) {
@@ -62,6 +71,8 @@ public class JpaConfigurator {
 		props.setProperty("hibernate.cache.use_second_level_cache", "true");
 		props.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory");
 		props.setProperty("hibernate.cache.use_query_cache", "true");
+		props.setProperty("hibernate.generate_statistics", "true");
+
 
 		entityManagerFactory.setJpaProperties(props);
 		return entityManagerFactory;
